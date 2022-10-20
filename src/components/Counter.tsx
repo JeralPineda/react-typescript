8d22244c46b97feea427ceed1657c0cd1e100b07
Jeral Pineda <jeral.cris@gmail.com>
2022-10-19 21:10:03 -0600

    useState con typescript

---
+++ src/components/Counter.tsx
@@ -0,0 +1,41 @@
+import { useState } from 'react';
+
+export const Counter = () => {
+  const [counter, setCounter] = useState(0);
+
+  const increment = (numero: number = 1): void => {
+    setCounter(counter + numero);
+  };
+
+  return (
+    <div className="mt-5">
+      <h1>Counter: useState</h1>
+      <span>Valor: {counter}</span>
+      <hr />
+
+      <button
+        //
+        className="btn btn-outline-primary mt-2"
+        onClick={() => increment()}
+      >
+        +1
+      </button>
+
+      <button
+        //
+        className="btn btn-outline-primary mt-2"
+        onClick={() => increment(2)}
+      >
+        +1
+      </button>
+
+      <button
+        //
+        className="btn btn-outline-danger mt-2"
+        onClick={() => setCounter(0)}
+      >
+        Reset
+      </button>
+    </div>
+  );
+};
